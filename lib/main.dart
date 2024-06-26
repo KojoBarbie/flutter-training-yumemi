c35103d084ab5fcd85f1ca61ce770c716d910567
KojoBarbie <tmkhsy-0420@g.ecc.u-tokyo.ac.jp>
2024-06-26 22:24:31 +0900

    feat: session1-1

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,67 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: Column(
+              // mainAxisAlignmentで合わせると、矩形が正しく中央にこない
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const AspectRatio(
+                  aspectRatio: 1,
+                  child: Placeholder(),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: Text(
+                        '** ℃',
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                              color: Colors.blue,
+                            ),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: Text(
+                        '** ℃',
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                              color: Colors.red,
+                            ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          debugPrint('Close');
+                        },
+                        child: const Text('Close'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          debugPrint('Reload');
+                        },
+                        child: const Text('Reload'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
